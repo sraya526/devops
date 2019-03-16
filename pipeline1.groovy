608fc60f758a060de4c13cfa0b4ab0e7bd6e4c8e
sraya526 <42809640+sraya526@users.noreply.github.com>
2019-03-16 22:30:16 +0530

    Create pipeline1.groovy

---
+++ pipeline1.groovy
@@ -0,0 +1,11 @@
+node {
+  stage('Target-id') {
+  sh ('aws elbv2 describe-target-groups --region ap-south-1 --load-balancer-arn arn:aws:elasticloadbalancing:ap-south-1:724016494080:loadbalancer/app/Flipgo-ALB/7b2381190f854f37')
+  }
+  stage('TG') {
+  sh '/var/lib/jenkins/TG.sh'
+  }
+  stage('IP') {
+  sh 'cd /var/lib/jenkins/ ; chown jenkins ip.sh ; chmod 700 ip.sh ; /var/lib/jenkins/ip.sh'      
+  }
+}
