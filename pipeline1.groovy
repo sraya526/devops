6850c2c89b1502d95dcc6104d36e2bee8a67814d
Your Name <you@example.com>
2019-03-16 17:08:59 +0000

    updated script

--- pipeline1.groovy
+++ pipeline1.groovy
@@ -1,5 +1,5 @@
 node {
-  stage('Target-id') {
+  stage('Target-ID') {
   sh ('aws elbv2 describe-target-groups --region ap-south-1 --load-balancer-arn arn:aws:elasticloadbalancing:ap-south-1:724016494080:loadbalancer/app/Flipgo-ALB/7b2381190f854f37')
   }
   stage('TG') {
